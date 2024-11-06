3ff762c348ae48add7953f882da3d39e7a21d8a0
sumonDev9 <sumonmit678@gmail.com>
2024-11-06 18:01:33 +0530

    statistics page updated

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -6,8 +6,15 @@ useEffect( () => {
         document.title ="Statistics | Gadget Heaven";
     },[]);
     return (
+         <div>
+             <div className="bg-primary text-center pt-8 pb-10">
+                <h1 className="text-white font-bold text-3xl">Statistics</h1>
+                <p className="text-white max-w-3xl mx-auto mt-4">Statistics is the study of collecting, analyzing, interpreting, presenting, and organizing data to uncover patterns and inform decisions.</p>
+            </div>
+         
         <div className="flex w-11/12 mx-auto min-h-96 mt-8 rounded-md justify-center items-center border-2 border-[rgba(9, 8, 15, 0.1)]">
             <h1 className="text-primary text-5xl text-center font-bold">No Data Found</h1>
+        </div>
         </div>
     );
 };
